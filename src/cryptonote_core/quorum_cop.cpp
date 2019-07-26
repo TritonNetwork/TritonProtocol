a2db5bb1d09c4bd4a68d6efcdb059cfe7252deb7
Harrison Hesslink <harrisonhesslink24@gmail.com>
2019-07-25 21:36:14 -0700

    Subtract 1 on generation

--- src/cryptonote_core/quorum_cop.cpp
+++ src/cryptonote_core/quorum_cop.cpp
@@ -272,7 +272,7 @@ namespace service_nodes
 	{
 		ribbon_protocol rp(m_core);
 		req.timestamp = time(nullptr);
-		req.height = m_core.get_current_blockchain_height();
+		req.height = m_core.get_current_blockchain_height() - 1;
 
 		std::vector<service_nodes::exchange_trade> recent_trades = rp.trades_during_latest_1_block();
 
