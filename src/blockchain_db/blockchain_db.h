eeb1658a60645f6de18158992e061085b942946b
Harrison Hesslink <harrisonhesslink24@gmail.com>
2020-12-16 14:06:43 -0800

    Remove old db oracle data

--- src/blockchain_db/blockchain_db.h
+++ src/blockchain_db/blockchain_db.h
@@ -1855,11 +1855,6 @@ public:
   virtual bool get_service_node_data(std::string& data) = 0;
   virtual void clear_service_node_data() = 0;
 
-
-  virtual void set_oracle_data(const std::string& data, const uint64_t &height) = 0;
-  virtual bool get_oracle_data(std::string& data, const uint64_t &height) = 0;
-  virtual void clear_oracle_data() = 0;
-
   /**
    * @brief set whether or not to automatically remove logs
    *
